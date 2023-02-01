f8d4fdb1771367c430703ee01ded5b273a4a0734
savi256 <isaacchidera256@gmail.com>
2023-02-01 20:23:08 +0100

    first commitc

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+  //APP
 }
 
 export default MyApp
